71db8ab0bef8584df54a23785ed10b8e0f9e4334
thanhdan0811 <ngthdan0811@gmail.com>
2021-05-27 23:25:08 +0700

    set up

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -6,54 +6,58 @@ import React from 'react';
 class Square extends React.Component {
   render() {
     return (
-      <button>
+      <button className="square">
 
       </button>
     )
   }
 }
 class Board extends React.Component {
-  renderSquard() {
-    retuen <Square />;
+  renderSquare() {
+    return <Square />;
   }
 
   render() {
+    let status = "Next player: X"
     return (
-      <button>
-
-      </button>
+      <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+      </div>
     )
   }
 }
 class Game extends React.Component {
   render() {
     return (
-      <button>
-        
-      </button>
-    )
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
   }
 }
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Game/>;
 }
 
 export default App;
